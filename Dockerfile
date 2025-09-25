30b25ff16632f6185631778f1e7379f604f7e6e8
Francisco Adasme <francisco.adasme@gmail.com>
2025-09-25 19:45:20 -0300

    refactor: Run softepigen as external executable

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY ./shard.yml ./shard.lock /app/
 RUN shards install --frozen
 
 COPY ./ /app/
+RUN crystal build --no-debug --release --static -o /app/bin/softepigen /app/lib/softepigen/src/main.cr
 RUN shards build --no-debug --release --static web
+RUN mkdir -p /app/public/output
 
 CMD ["/app/bin/web"]
--- src/app.cr
+++ src/app.cr
@@ -21,16 +21,35 @@ post "/upload" do |env|
   cpg_min = env.params.body["cpg_min"].to_i? || halt(env, 400, "Invalid cpg")
   cpg_max = env.params.body["cpg_max"].to_i? || halt(env, 400, "Invalid cpg")
   allowed_cpg = cpg_min..cpg_max
-  kmer = env.params.body["astringent"]? ? 5 : 4
+  astringent = env.params.body["astringent"]? ? 1 : 0
 
   begin
     Log.info { "Processing file #{file_upload.filename}..." }
-    file_upload.tempfile.rewind
-    amplicons = Softepigen.find_amplicons file_upload.tempfile, primer_size, amplicon_size, allowed_cpg, kmer
-    amplicons = Softepigen.fold_amplicons(amplicons)
-    file_upload.tempfile.rewind
-    chromosome = file_upload.tempfile.read_line.lchop('>').split(/[\-:]/)[0]
-    bed_content = String.build { |io| Softepigen.write_bed io, chromosome, amplicons }
+
+    timestamp = Time.local.to_s("%Y%m%d%H%M%S")
+    stem = File.basename Path[file_upload.filename.not_nil!].stem, ".fasta"
+    output = "public/output/#{timestamp}-#{stem}"
+    bed_file = "#{output}-out.bed"
+
+    args = [
+      "--amplicon=#{amplicon_size.to_s.sub("..", ",")}",
+      "--primer=#{primer_size.to_s.sub("..", ",")}",
+      "--cpg=#{allowed_cpg.to_s.sub("..", ",")}",
+      "--astringency=#{astringent}",
+      "--output=#{output}",
+      file_upload.tempfile.path,
+    ]
+    io = IO::Memory.new
+    status = Process.run "./bin/softepigen", args, output: io, error: io
+    if status.success?
+      bed_content = File.exists?(bed_file) ? File.read(bed_file) : ""
+      chromosome = bed_content[/browser position (chr\d+)/, 1]? || "chr??"
+      amplicons = `tail -1 #{bed_file}`[/Neg(\d+)/, 1]? || 0
+    else
+      message = io.to_s.strip
+      Log.error { "Softepigen failed: #{message}" }
+      halt env, 500, "Failed to process file due to #{message}"
+    end
   rescue err : Exception
     Log.error exception: err
     halt env, 500, "Failed to process file due to #{err}"
@@ -38,18 +57,7 @@ post "/upload" do |env|
 
   env.response.headers["Content-Type"] = "application/json"
   {
-    "amplicons" => amplicons.map do |amp|
-      {
-        "start":    amp.forward_primer.start,
-        "stop":     amp.reverse_primer.stop,
-        "size":     amp.size,
-        "sequence": amp.to_s,
-        "primers":  {
-          "forward": amp.forward_primer.to_s,
-          "reverse": amp.reverse_primer.to_s,
-        },
-      }
-    end,
+    "amplicons"  => amplicons,
     "bed"        => bed_content,
     "chromosome" => chromosome,
   }.to_json
